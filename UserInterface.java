547ce585081086be0b966836f6bb52c2ecd93064
ghimirelava <89549407+ghimirelava@users.noreply.github.com>
2023-04-01 22:30:40 -0700

    Add files via upload

--- UserInterface.java
+++ UserInterface.java
@@ -40,7 +40,7 @@ public class UserInterface{
             System.out.println();
         }
 	}
-	public void delete(String filename, String key, String type) throws IOException {
+	public void delete(String filename, String search_value, String type) throws IOException {
 		EmailCategorizer categorizer = new EmailCategorizer();
         BufferedReader reader = new BufferedReader(new FileReader(filename));
         reader.readLine();
@@ -61,8 +61,12 @@ public class UserInterface{
             Map.put(category, emailList);
         }
         reader.close();
-
-        Map = deleteEmailAndKeywords(Map,key,null);
+        
+        if (type.equals("type1")) {
+        	Map = deleteEmailAndKeywords(Map,search_value,null,type);
+        }else {
+        	Map = deleteEmailAndKeywords(Map,null,search_value,type);
+        }
         
         // Printing
         for (String category : Map.keySet()) {
@@ -75,19 +79,19 @@ public class UserInterface{
         }
 	}
 	
-	public static Map<String, List<String>> deleteEmailAndKeywords(Map<String, List<String>> map, String Email, String keyword) {
+	public static Map<String, List<String>> deleteEmailAndKeywords(Map<String, List<String>> map, String emailAddress, String keyword,  String type) {
         Map<String, List<String>> updatedMap = new HashMap<>();
         List<String> emailSubjectList = new ArrayList<>();
         for (String category : map.keySet()) {
             for (String emailSubject : map.get(category)) {
                 String email = emailSubject.split(":")[0].trim();
                 // Delete by email
-                if(Email != null && email.equals(Email)){
+                if(emailAddress != null && email.equals(emailAddress) && type.equals("type1")){
                     continue;
                 }
-                    if(keyword != null && emailSubject.toLowerCase().contains(keyword.toLowerCase())){
+                if(keyword != null && emailSubject.toLowerCase().contains(keyword.toLowerCase()) && type.equals("type2")){
                       continue;
-                    }
+                }
                 emailSubjectList.add(emailSubject);
             }
             updatedMap.put(category, emailSubjectList);
@@ -129,13 +133,13 @@ public class UserInterface{
 			else {
 	
 				if (input.equals("1")) {
-		            System.out.println("Enter 1 to delete by KEYWORD or EMAIL ADDRESS");
-		            System.out.println("      2 to delete by EMAIL ADDRESS");
+		            System.out.println("Enter 1 to delete by EMAIL ADDRESS");
+		            System.out.println("      2 to delete by KEYWORD");
 		            System.out.println("      3 to delete by CATEGORY");
 		            input = sc.nextLine();
 		            
 		            if (input.equals("1")) {
-		            	System.out.println("Enter keywords/ email address: ");
+		            	System.out.println("Enter email address: ");
 		            	input = sc.nextLine();
 		            	
 		            	// calls function with filename and input --> for each input compares it with the elems in the file and deletes them
@@ -143,7 +147,7 @@ public class UserInterface{
 		            	//outputs previous count, number of emails deleted, current count and save new inbox in "newInbox.txt"
 		            	
 		            }else if (input.equals("2")) {
-		            	System.out.println("Enter email address(s): ");
+		            	System.out.println("Enter keyword: ");
 		            	input = sc.nextLine();
 		            	
 		            	// calls function with filename and input --> for each input compares it with the elems in the file and deletes them
